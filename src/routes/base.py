fd4edc31e29f571054d2ef3782f89d86831daa08
Mahmoud Abdelrazek <razekmh@gmail.com>
2025-12-26 01:31:07 +0000

    use depends
    Signed-off-by: Mahmoud Abdelrazek <razekmh@gmail.com>

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from helpers.config import get_settings
 
 base_router = APIRouter(prefix="/api/v1", tags=["api_v1"])
 
 
 @base_router.get("/")
-async def welcome():
+async def welcome(app_settings=Depends(get_settings)):
     app_settings = get_settings()
     return {
         "message": f"Welcome to the {app_settings.APP_NAME} API! {app_settings.APP_VERSION}"
