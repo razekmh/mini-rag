b1a67df41eec9a41038777af214b187b7b11324d
Mahmoud Abdelrazek <razekmh@gmail.com>
2025-12-28 03:06:33 +0000

    add generate random key and save file
    Signed-off-by: Mahmoud Abdelrazek <razekmh@gmail.com>

--- src/controllers/FileController.py
+++ src/controllers/FileController.py
@@ -1,6 +1,11 @@
-from .BaseController import BaseController
+import re
+
 from fastapi import UploadFile
+
 from models import ResoponseSignal
+
+from .BaseController import BaseController
+from .ProjectController import ProjectController
 
 
 class FileController(BaseController):
@@ -14,3 +19,28 @@ class FileController(BaseController):
         if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
             return False, ResoponseSignal.FILE_SIZE_EXCEEDED.value
         return True, ResoponseSignal.FILE_UPLOAD_SUCCESS.value
+
+    def generate_unique_filename(
+        self, orig_file_name: str | None, project_id: str
+    ) -> str:
+        random_key = self.generate_random_string()
+        project_path = ProjectController().get_project_path(project_id=project_id)
+        cleaned_file_name = self.get_clean_file_name(orig_file_name=orig_file_name)
+
+        new_file_path = project_path / (random_key + "_" + cleaned_file_name)
+
+        while new_file_path.is_file():
+            random_key = self.generate_random_string()
+            new_file_path = project_path / (random_key + "_" + cleaned_file_name)
+
+        return new_file_path
+
+    def get_clean_file_name(self, orig_file_name: str | None) -> str | None:
+        if not orig_file_name:
+            return None
+
+        cleaned_file_name = re.sub(r"[ˆ\w.]", "", orig_file_name.strip())
+
+        cleaned_file_name = cleaned_file_name.replace(" ", "_")
+
+        return cleaned_file_name
--- src/routes/files.py
+++ src/routes/files.py
@@ -1,9 +1,14 @@
+import logging
+
 import aiofiles
 from fastapi import APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 
-from controllers import FileController, ProjectController
+from controllers import FileController
 from helpers.config import Settings, get_settings
+from models import ResoponseSignal
+
+logger = logging.getLogger("uvicorn.error")
 
 file_router = APIRouter(prefix="/api/v1/files", tags=["api_v1", "files"])
 
@@ -14,15 +19,26 @@ async def upload_file(
     file: UploadFile,
     app_settings: Settings = Depends(get_settings),
 ) -> JSONResponse:
-    is_valid, result_signal = FileController().validate_uploaded_file(file=file)
+    file_controller = FileController()
+    is_valid, result_signal = file_controller.validate_uploaded_file(file=file)
     if not is_valid:
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST, content={"signal": result_signal}
         )
-    project_dir_path = ProjectController().get_project_path(project_id=project_id)
-    file_path = project_dir_path / file.filename
-    async with aiofiles.open(file_path, "wb") as f:
-        while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
-            await f.write(chunk)
 
-    return JSONResponse(content={"signal": result_signal})
+    unique_filepath = file_controller.generate_unique_filename(
+        orig_file_name=file.filename, project_id=project_id
+    )
+    try:
+        async with aiofiles.open(unique_filepath, "wb") as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception as e:
+        logger.error(f"Error while uploading file: {e}")
+
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"signal": ResoponseSignal.FILE_UPLOAD_FAILED},
+        )
+
+    return JSONResponse(content={"signal": ResoponseSignal.FILE_UPLOAD_SUCCESS})
