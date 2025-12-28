eccda27a53764e9c0bf18f1c90c2bec58b817bda
Mahmoud Abdelrazek <razekmh@gmail.com>
2025-12-28 02:16:45 +0000

    fix parents bug
    Signed-off-by: Mahmoud Abdelrazek <razekmh@gmail.com>

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -6,5 +6,5 @@ from pathlib import Path
 class BaseController(ABC):
     def __init__(self):
         self.app_settings = get_settings()
-        self.base_dir = Path(__file__)
+        self.base_dir = Path(__file__).parent.parent
         self.files_dir = self.base_dir / "assets" / "files"
