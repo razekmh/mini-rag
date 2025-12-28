8a5fb7ab2c308bccdacc86cdb20ba501fe411a52
Mahmoud Abdelrazek <razekmh@gmail.com>
2025-12-28 01:21:23 +0000

    add vars
    Signed-off-by: Mahmoud Abdelrazek <razekmh@gmail.com>

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -6,6 +6,9 @@ class Settings(BaseSettings):
     APP_VERSION: str
     OPENAI_API_KEY: str
 
+    FILE_ALLOWED_TYPES: list
+    FILE_MAX_SIZE: int
+
     class Config:
         env_file = ".env"
 
--- src/routes/base.py
+++ src/routes/base.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends
 from helpers.config import get_settings, Settings
+from typing import Dict
 
 base_router = APIRouter(prefix="/api/v1", tags=["api_v1"])
 
 
 @base_router.get("/")
-async def welcome(app_settings: Settings = Depends(get_settings)):
+async def welcome(app_settings: Settings = Depends(get_settings)) -> Dict[str, str]:
     app_settings = get_settings()
     return {
         "message": f"Welcome to the {app_settings.APP_NAME} API! {app_settings.APP_VERSION}"
